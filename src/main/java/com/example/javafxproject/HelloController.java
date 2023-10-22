c88512e91170b03dd3c97d6b8b661fd2e95f4071
Ariel Dobkin <arieldob13@gmail.com>
2023-10-22 13:16:10 +0300

    first commit

--- src/main/java/com/example/javafxproject/HelloController.java
+++ src/main/java/com/example/javafxproject/HelloController.java
@@ -1,17 +1,23 @@
 package com.example.javafxproject;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class HelloController {
+
     @FXML
-    private Label welcomeText;
+    private ImageView image;
+
     @FXML
-    ImageView image;
+    private Button welcomeText;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-        image.setVisible(true);
+    public void initialize() {
+        // Load the image from your project's resources
+        Image image1 = new Image(getClass().getResourceAsStream("..\\..\\..\\..\\java\\com\\example\\javafxproject\\images\\9-black.png"));
+
+        // Set the image to the ImageView
+        image.setImage(image1);
     }
 }
